b4f0bb4f387518faff80bc7a1e390ede2e22dd8c
Codacy Archive <codacy-archive@local>
2026-03-19 01:21:34 +0000

    snapshot 2026-03-19T01:21:34.289200+00:00

--- audisp/plugins/ids/reactions.c
+++ audisp/plugins/ids/reactions.c
@@ -35,6 +35,7 @@ static int safe_exec(const char *exe, ...)
 	unsigned int i;
 	int pid;
 	struct sigaction sa;
+	sigset_t nmask, omask;
 
 	if (exe == NULL) {
 		syslog(LOG_ALERT,
@@ -42,16 +43,30 @@ static int safe_exec(const char *exe, ...)
 		return 1;
 	}
 
+	sigemptyset(&nmask);
+	sigaddset(&nmask, SIGCHLD);
+	if (sigprocmask(SIG_BLOCK, &nmask, &omask) < 0) {
+		syslog(LOG_ALERT,
+			"Audit IDS failed to block SIGCHLD for %s", exe);
+		return 1;
+	}
+
 	pid = fork();
 	if (pid < 0) {
 		syslog(LOG_ALERT,
 			"Audit IDS failed to fork doing safe_exec");
+		sigprocmask(SIG_SETMASK, &omask, NULL);
 		return 1;
 	}
 	if (pid) {       /* Parent */
 		int status;
+		int rc;
 
-		if (waitpid(pid, &status, 0) < 0) {
+		do {
+			rc = waitpid(pid, &status, 0);
+		} while (rc < 0 && errno == EINTR);
+		sigprocmask(SIG_SETMASK, &omask, NULL);
+		if (rc < 0) {
 			syslog(LOG_ALERT,
 				"Audit IDS waitpid failed for %s", exe);
 			return 1;
@@ -64,6 +79,7 @@ static int safe_exec(const char *exe, ...)
 	}
 
 	/* Child */
+	sigprocmask(SIG_SETMASK, &omask, NULL);
 	sigfillset (&sa.sa_mask);
 	sigprocmask (SIG_UNBLOCK, &sa.sa_mask, 0);
 #ifdef HAVE_CLOSE_RANGE
@@ -324,10 +340,25 @@ int system_halt(void)
 	return safe_exec("/sbin/init", "0");
 }
 
+/*
+ * get_reaction_account - fetch account name for current triggering session
+ * Returns: account name from current session, or NULL if unavailable
+ */
+static const char *get_reaction_account(void)
+{
+	session_data_t *s = current_session();
+
+	if (s == NULL)
+		return NULL;
+
+	return s->acct;
+}
+
 void do_reaction(unsigned int answer, const char *reason)
 {
 //my_printf("Answer: %u", answer);
 	unsigned int num = 0;
+	const char *acct = get_reaction_account();
 
 	do {
 		unsigned int tmp = 1 << num;
@@ -357,31 +388,27 @@ void do_reaction(unsigned int answer, const char *reason)
 					break;
 				case REACTION_RESTRICT_ROLE:
 					{
-					account_data_t *a = current_account();
-					if (a)
-					    restricted_role(a->name);
+					if (acct)
+						restricted_role(acct);
 					}
 					break;
 				case REACTION_PASSWORD_RESET:
 					{
-					account_data_t *a = current_account();
-					if (a)
-					    force_password_reset(a->name);
+					if (acct)
+						force_password_reset(acct);
 					}
 					break;
 				case REACTION_LOCK_ACCOUNT_TIMED:
 					{
-					account_data_t *a = current_account();
-					if (a)
-					    lock_account_timed(a->name,
+					if (acct)
+						lock_account_timed(acct,
 						config.lock_account_time);
 					}
 					break;
 				case REACTION_LOCK_ACCOUNT:
 					{
-					account_data_t *a = current_account();
-					if (a)
-					    lock_account(a->name);
+					if (acct)
+						lock_account(acct);
 					}
 					break;
 				case REACTION_BLOCK_ADDRESS_TIMED:
@@ -407,4 +434,3 @@ void do_reaction(unsigned int answer, const char *reason)
 		num++;
 	} while (num < 32);
 }
-
--- auplugin/auplugin.c
+++ auplugin/auplugin.c
@@ -127,14 +127,18 @@ static void common_inbound(void)
 			int len;
 			if ((len = auplugin_fgets(rx_buf,
 				    MAX_AUDIT_EVENT_FRAME_SIZE + 1, fd)) > 0) {
+				size_t copy_len;
+
 				// Got one - enqueue it
 				event_t *e = (event_t *)calloc(1,
 					 sizeof(event_t));
 				if (e) {
-					strncpy(e->data, rx_buf,
-						MAX_AUDIT_MESSAGE_LENGTH);
-					e->data[MAX_AUDIT_MESSAGE_LENGTH-1] = 0;
-					e->hdr.size = len;
+					copy_len = (size_t)len;
+					if (copy_len >= MAX_AUDIT_MESSAGE_LENGTH)
+						copy_len = MAX_AUDIT_MESSAGE_LENGTH - 1;
+					memcpy(e->data, rx_buf, copy_len);
+					e->data[copy_len] = 0;
+					e->hdr.size = copy_len;
 					e->hdr.ver = AUDISP_PROTOCOL_VER2;
 					enqueue(e, &q_config);
 				}
@@ -213,9 +217,15 @@ int auplugin_event_feed(auparse_callback_ptr callback, unsigned int t_interval,
 
 		return -1;
 	}
-	pthread_detach(outbound_thread);
 
 	common_inbound();
+
+	rc = pthread_join(outbound_thread, NULL);
+	if (rc) {
+		syslog(LOG_ERR, "pthread_join failed: %d", rc);
+		return -1;
+	}
+
 	return 0;
 }
 
@@ -343,4 +353,3 @@ int auplugin_queue_overflow(void)
 {
 	return queue_overflowed_p();
 }
-
